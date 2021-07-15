aa0863bcaa003708aa3bda2ee3ef72bd5b7b4a91
Gabriel Ayala <gabeayala100@gmail.com>
2021-07-15 12:50:12 -0700

    Add a toppingsPrice function that passes the third test.

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,36 @@ function Pizza(toppings, size) {
   this.toppings = toppings;
   this.size = size;
 }
-
 let testPizza = new Pizza(["pepperoni", "bellpeppers","garlic drizzle"], "Earthling");
 
-Pizza.prototype.sizePrice = function() {
-  if(this.size === 'Multidimensional') {
-    return '$70.00';
-  } else if(this.size === 'Universal') {
-    return '$55.00';
-  } else if(this.size === 'Galactic') {
-    return '$40.00';
-  } else if(this.size === 'Earthling') {
-    return '$25.00'
-  }
+Pizza.prototype.toppingsPrice = function() {
+  let toppingsPrice = 0;
+
+  for(let i = 0; i <this.toppings.length; i++ ) {
+    toppingsPrice += 2;
+  };
+  return toppingsPrice;
 }
 
-testPizza.sizePrice();
+Pizza.prototype.sizePrice = function() {
+  let sizePrice =0;
+  if(this.size === 'Multidimensional') {
+    sizePrice = 70;
+  } else if(this.size === 'Universal') {
+    sizePrice = 55.00;
+  } else if(this.size === 'Galactic') {
+    sizePrice = 40.00;
+  } else if(this.size === 'Earthling') {
+    sizePrice = 25.00;
+  }
+  
+}
 
+
+Pizza.prototype.totalPrice = function() {
+  return (toppingsPrice + sizePrice);
+
+}
+
+testPizza.totalPrice();
 
