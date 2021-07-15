a6fe3084e6a42c1b76d989d7bd554a147f6b5229
Gabriel Ayala <gabeayala100@gmail.com>
2021-07-15 11:54:58 -0700

    Create a function that will pass the test within the sizePrice.

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,20 @@ function Pizza(toppings, size) {
   this.size = size;
 }
 
-let testPizza = new Pizza(["pepperoni", "bellpeppers","garlic drizzle"], "large");
+let testPizza = new Pizza(["pepperoni", "bellpeppers","garlic drizzle"], "Earthling");
+
+Pizza.prototype.sizePrice = function() {
+  if(this.size === 'Multidimensional') {
+    return '$70.00';
+  } else if(this.size === 'Universal') {
+    return '$55.00';
+  } else if(this.size === 'Galactic') {
+    return '$40.00';
+  } else if(this.size === 'Earthling') {
+    return '$25.00'
+  }
+}
+
+testPizza.sizePrice();
+
 
